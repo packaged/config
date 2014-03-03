4e4a0b552fd1487c7ee7722e636efb4bf345c994
Brooke Bryan <brooke@bajb.net>
2014-03-03 17:41:24 +0000

    Config Section now implements ArrayAccess

--- src/Provider/ConfigSection.php
+++ src/Provider/ConfigSection.php
@@ -6,7 +6,7 @@ use Packaged\Config\ConfigSectionInterface;
 /**
  * Configuration section
  */
-class ConfigSection implements ConfigSectionInterface
+class ConfigSection implements ConfigSectionInterface, \ArrayAccess
 {
   protected $_name;
   protected $_items;
@@ -74,5 +74,75 @@ class ConfigSection implements ConfigSectionInterface
   {
     $this->_items[$key] = $value;
     return $this;
+  }
+
+  /**
+   * (PHP 5 &gt;= 5.0.0)<br/>
+   * Whether a offset exists
+   * @link http://php.net/manual/en/arrayaccess.offsetexists.php
+   *
+   * @param mixed $offset <p>
+   *                      An offset to check for.
+   *                      </p>
+   *
+   * @return boolean true on success or false on failure.
+   * </p>
+   * <p>
+   * The return value will be casted to boolean if non-boolean was returned.
+   */
+  public function offsetExists($offset)
+  {
+    return isset($this->_items[$offset]);
+  }
+
+  /**
+   * (PHP 5 &gt;= 5.0.0)<br/>
+   * Offset to retrieve
+   * @link http://php.net/manual/en/arrayaccess.offsetget.php
+   *
+   * @param mixed $offset <p>
+   *                      The offset to retrieve.
+   *                      </p>
+   *
+   * @return mixed Can return all value types.
+   */
+  public function offsetGet($offset)
+  {
+    return $this->getItem($offset);
+  }
+
+  /**
+   * (PHP 5 &gt;= 5.0.0)<br/>
+   * Offset to set
+   * @link http://php.net/manual/en/arrayaccess.offsetset.php
+   *
+   * @param mixed $offset <p>
+   *                      The offset to assign the value to.
+   *                      </p>
+   * @param mixed $value  <p>
+   *                      The value to set.
+   *                      </p>
+   *
+   * @return void
+   */
+  public function offsetSet($offset, $value)
+  {
+    $this->addItem($offset, $value);
+  }
+
+  /**
+   * (PHP 5 &gt;= 5.0.0)<br/>
+   * Offset to unset
+   * @link http://php.net/manual/en/arrayaccess.offsetunset.php
+   *
+   * @param mixed $offset <p>
+   *                      The offset to unset.
+   *                      </p>
+   *
+   * @return void
+   */
+  public function offsetUnset($offset)
+  {
+    unset($this->_items[$offset]);
   }
 }
--- tests/ConfigSectionTest.php
+++ tests/ConfigSectionTest.php
@@ -9,4 +9,17 @@ class ConfigSectionTest extends ConfigSectionBaseTest
   {
     return new \Packaged\Config\Provider\ConfigSection();
   }
+
+  public function testArrayAccess()
+  {
+    $section = $this->getConfigSection();
+    $this->assertFalse(isset($section['random']));
+    $this->assertNull($section['random']);
+    $section['random'] = 'testing';
+    $this->assertTrue(isset($section['random']));
+    $this->assertEquals('testing', $section['random']);
+    $this->assertEquals('testing', $section->getItem('random'));
+    unset($section['random']);
+    $this->assertFalse(isset($section['random']));
+  }
 }
